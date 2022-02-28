0086745108e2c6f98823ac52ffb6a01ad94b59fc
William Foucher <foucher.william1@cmaisonneuve.qc.ca>
2022-02-28 09:50:09 -0500

    barre de recherche

---
+++ search.php
@@ -0,0 +1,28 @@
+
+<?php
+/**
+ * le modèle de recherche search.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package cidw-4w4
+ */
+ get_header();?>
+<main class="site__main">
+    <section class="site__main__recherche">
+        <h1>Résultat de recherche</h1>
+    </section>
+    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+        <a href="<?php echo get_permalink();?>">   
+            <h2><?php the_title() ?></h2> 
+        </a>
+        <p>
+            <?php echo wp_trim_words(get_the_content(),20); ?>
+        </p>
+        <hr>
+    <?php endwhile ?>
+    <?php else : ?>
+        <h2>Aucun résultat pour la recherche</h2>
+    <?php endif ?>
+</main>
+<?php get_footer(); ?>

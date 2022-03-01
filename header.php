ff8d9d98ec3d9329d8bbc9491a9f7dda8ee3416e
William Foucher <76762472+willfou193@users.noreply.github.com>
2022-03-01 12:54:58 -0500

    debut hyperlien du titre

--- header.php
+++ header.php
@@ -40,10 +40,10 @@
 <section class="site__barre">
     <input type="checkbox" id="chk-burger">
     <label for="chk-burger" id="burger">
-    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>''
+    <svg  width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>''
     </label>
     <?php 
-    $iconeMenu = '<svg width="14px" height="14px" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="#000000" viewBox="0 0 24 24"><polygon fill-rule="evenodd" points="8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4"></polygon></svg>';
+    $iconeMenu = '<svg class="iconeMenu" width="22px" height="22px" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="#000000" viewBox="0 0 24 24"><polygon fill-rule="evenodd" points="8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4"></polygon></svg>';
     wp_nav_menu(array("principal", 'link_before' => "$iconeMenu")); ?>
     
 </section>

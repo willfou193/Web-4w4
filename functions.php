275d81f6f23f818f65eba14276e67b025d76fd15
William Foucher <foucher.william1@gmail.com>
2022-03-10 14:33:43 -0500

    fonction sidebar avec fichier s'y rapportant

--- functions.php
+++ functions.php
@@ -33,4 +33,23 @@ function cidw_4w4_filtre_choix_menu($obj_menu){
 add_filter("wp_nav_menu_objects","cidw_4w4_filtre_choix_menu");
 // ---------------------------------------------------------------------add_theme support()
 add_theme_support( 'post-thumbnails' );
+
+
+// -------------------------------------------------------------------Enregistrement
+add_action( 'widgets_init', 'my_register_sidebars' );
+function my_register_sidebars() {
+    /* Register the 'primary' sidebar. */
+    register_sidebar(
+        array(
+            'id'            => 'footer_colonne1',
+            'name'          => __( 'Footer colonne1' ),
+            'description'   => __( 'Ce sidebar s\'affiche dans une colonne de pied de page.' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+    /* Repeat register_sidebar() code for additional sidebars. */
+}
 ?>

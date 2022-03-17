1f293cd908c76fea4bc0ed3dddcd956a374b1152
William Foucher <foucher.william1@gmail.com>
2022-03-17 19:37:53 -0400

    semaine 8 c2 tard, footer presque finis, reste à finir le mode mobile

--- footer.php
+++ footer.php
@@ -3,7 +3,9 @@
     <h3 class="footer__author">Fait (en parti) par William Foucher</h3>
     <!-- premier groupe de colonne (desktop) -->
     <div class="footer__conteneur">
+        
         <div class="footer__widget">
+            <h2>Derniers articles</h2>
             <?php dynamic_sidebar( 'footer_colonne1' ); ?>
         </div>
         
@@ -11,6 +13,7 @@
             <?php get_search_form() ?>
         </div>
         <div class="footer__page">
+            <h2>Pages annexes</h2>
             <?php 
                 $icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#f00"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
                 wp_nav_menu(array(
@@ -30,9 +33,11 @@
                 <?php dynamic_sidebar( 'footer_conteneur2_colonne1' ); ?>
         </div>
         <div class="lien-externe">
+            <h2>Liens externe</h2>
             <?php
                 wp_nav_menu(array(
-                    "menu"=>"externe"));
+                    "menu"=>"externe",
+                    "link_before"=>$icone));
             ?>
         </div>
         <div class="footer__widget">

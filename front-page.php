6d65ecc77fe111dfd9f3e1c82c397f501666dde5
William Foucher <foucher.william1@gmail.com>
2022-03-17 18:33:44 -0400

    animation hover carte

--- front-page.php
+++ front-page.php
@@ -2,11 +2,9 @@
 <main class="site__main">
     <h1>Bienvenue!</h1>
     <section class="animation">
-        <div class="animation__bloc">1</div>
-        <div class="animation__bloc">2</div>
-        <div class="animation__bloc">3</div>
-        <div class="animation__bloc">4</div>
-        <div class="animation__bloc">5</div>
+        <div class="animation__bloc" id="etudiant"></div>
+        <div class="animation__bloc"></div>
+        <div class="animation__bloc" id="cours"></div>
     </section>
 
     <?php if (have_posts()): while(have_posts()) : the_post();?> 

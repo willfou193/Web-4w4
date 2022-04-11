1b4e88583cd3c7468da9438adb904da0ca0cbd13
William Foucher <foucher.william1@gmail.com>
2022-04-11 16:15:10 -0400

    Fin menu front-page + barre de recherche footer fixed

--- front-page.php
+++ front-page.php
@@ -7,15 +7,23 @@
         <div class="animation__bloc" id="cours"></div>
     </section>
     <!-- ----------------- Menu accueil -------------------- -->
-    <?php
-        wp_nav_menu(array("menu"=>"accueil",
-        "container"=>"nav"));
-    ?>
+    <div class="menu__accueil">
+        <h3>Le département TIM</h3>
+        <?php
+            wp_nav_menu(array("menu"=>"accueil",
+            "container"=>"nav"));
+        ?>
+    </div>
+    
     <!-- ------------------Menu Evenement-------------------- -->
-    <?php
-        wp_nav_menu(array("menu"=>"evenement",
-        "container"=>"nav"));
-    ?>
+    <div class="menu__evenement">
+        <h3>Événements</h3>
+        <?php
+            wp_nav_menu(array("menu"=>"evenement",
+            "container"=>"nav"));
+        ?>
+    </div>
+    
     <?php if (have_posts()): while(have_posts()) : the_post();?> 
         <?php the_title(); ?>
         <?php the_content(); ?>

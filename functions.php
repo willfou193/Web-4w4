a0d35313ee675e6dfd8070991ecab2c75b65b732
William Foucher <foucher.william1@gmail.com>
2022-04-21 15:54:14 -0400

    s12 c2 pause: ajout javascript debut

--- functions.php
+++ functions.php
@@ -1,8 +1,15 @@
 <?php 
 function cidw_4w4_enqueue(){
     //wp_enqueue_style('style_css', get_stylesheet_uri());
-    wp_enqueue_style('4w4-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
+    wp_enqueue_style('cidw-4w4-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
+    
     wp_enqueue_style('cidw-4w4-google-font', "https://fonts.googleapis.com/css2?family=Merriweather:ital@1&family=Roboto:wght@300&family=Rubik&display=swap",false );
+
+    wp_enqueue_script('cidw-4w4-boite-modale',
+                        get_template_directory_uri() . '/javascript/boite-modale.js',
+                        array(), 
+                        filemtime(get_template_directory() . '/javascript/boite-modale.js'), 
+                        true); //true pour intégrer le JS en bas du document
 
 }
 
---
+++ javascript/boite-modale.js
@@ -0,0 +1,3 @@
+(function(){
+    console.log("tout va bien ici");
+})()

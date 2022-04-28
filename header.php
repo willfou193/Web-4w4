439b4cf63052776e7ccf6f8b1dcfae71e37eca08
William Foucher <foucher.william1@gmail.com>
2022-04-28 15:20:41 -0400

    s13 c1 finalisation de l'apparence (lab7) début tp2

--- header.php
+++ header.php
@@ -5,7 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
     <?php wp_head(); ?>
+    <style>
+        .site__header::after{
+        background-color:<?= get_theme_mod("background_clippath"); ?> ;
+    }
+    </style>
 </head>
 <body <?php body_class('site'); ?> style="background-color:<?= get_theme_mod("background_body"); ?>;">
 <header class="site__header">

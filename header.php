4e52724cc3f76807a1722b9d0ce4225ef269fb4b
William Foucher <foucher.william1@gmail.com>
2022-04-28 16:58:17 -0400

    s13 c1 menu burger clippath

--- header.php
+++ header.php
@@ -41,8 +41,10 @@
 
 <section class="site__barre">
     <input type="checkbox" id="chk-burger">
-    <label for="chk-burger" id="burger">
-    <svg  width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+    <label for="chk-burger" id="burger" class="burger">
+    <div class="burger__ligne"></div>
+    <div class="burger__ligne"></div>
+    <div class="burger__ligne"></div>
     </label>
     <?php 
     $iconeMenu = '<svg class="iconeMenu" width="22px" height="22px" xmlns="http://www.w3.org/2000/svg" fill="currentColor" color="#000000" viewBox="0 0 24 24"><polygon fill-rule="evenodd" points="8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4"></polygon></svg>';

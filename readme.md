85a8a89d2f3350fff84cf4307565023a5a8da666
William Foucher <foucher.william1@gmail.com>
2022-05-19 15:03:32 -0400

    ef-WF correction du read-me ainsi que la mise a jour des informations du theme

--- readme.md
+++ readme.md
@@ -1,12 +1,11 @@
 # tp2 - 4W4 - Conception d'interface et développement Web
 ### Auteur : William Foucher
-### Date de remise : 13 mai 2022
+### Date de remise : 19 mai 2022
 
 ```
-Version finale de mon site WordPress réalisé dans le cadre du cours 4W4. Merci pour ce cours qui me sera bien pratique pour mon stage
-cet été!
+Au cours de l'épreuve finale, j'ai ajouté un nouveau menu atelier a la front-page, créer un nouveau template sur wordpress et créer les fields avec custum field.
 
-Github page : https://github.com/willfou193/Web-4w4.git
+Github page : https://github.com/willfou193/Web-4w4/tree/epreuve-finale
 
 Pour modifier readme.md
 https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
--- template-atelier.php
+++ template-atelier.php
@@ -16,7 +16,7 @@ get_header();
         <h1><?php the_title(); ?></h1>
         <h3>Description de l'atelier</h3>
             <section class="evenement__animateur">
-            <p>L'animateur de l'atelier <div class="contenuField"><?php the_field('animateur');?></div></p>
+            <p>L'animateur de l'atelier : <div class="contenuField"><?php the_field('animateur');?></div></p>
             </section>
             <section class="evenement__local">
             <p> L'animateur de l'atelier :<div class="contenuField"><?php the_field('local_ou_se_deroulera_latelier');?></div></p>
@@ -27,19 +27,19 @@ get_header();
         
         <h3>Horaire et dates de l'atelier</h3>
         <section class="evenement__duree">
-                <p>Durée de chacune des séances est de <div class="contenuField"><?php the_field('duree_dune_seance_datelier'); ?></div></p>
+                <p>Durée de chacune des séances est de :<div class="contenuField"><?php the_field('duree_dune_seance_datelier'); ?></div></p>
         </section>
         <section class="evenement__debut">
-                <p>Date de début  <div class="contenuField"><?php the_field('date_de_debut_de_latelier'); ?></div></p>
+                <p>Date de début :  <div class="contenuField"><?php the_field('date_de_debut_de_latelier'); ?></div></p>
         </section>
         <section class="evenement__fin">
-                <p>Date de fin <div class="contenuField"><?php the_field('date_de_fin_de_latelier'); ?></div></p>    
+                <p>Date de fin : <div class="contenuField"><?php the_field('date_de_fin_de_latelier'); ?></div></p>    
         </section>
         <section class="evenement__jours">
                 <p>La formation se donnera : <div class="contenuField"><?php the_field('jours_de_la_semaine_de_latelier'); ?></div></p>
         </section>
         <section class="evenement__horaire">
-                <div>l'horaire :<div class="contenuField"><?php the_field('heure_de_debut');?> <p> à </p><?php the_field('heure_de_fin');?></div></div>
+                <p>l'horaire :<div class="contenuField"><?php the_field('heure_de_debut');?> <p> à </p><?php the_field('heure_de_fin');?></div></p>
         </section>
 
         <?php endif; ?>

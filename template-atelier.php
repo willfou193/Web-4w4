6a15b324c5a08329ce0b75704013b7e4f0552039
William Foucher <foucher.william1@gmail.com>
2022-05-19 14:36:19 -0400

    ef-WF Correction div résumer  ainsi que le flex des éléments de réponse des fields

--- template-atelier.php
+++ template-atelier.php
@@ -14,33 +14,33 @@ get_header();
     <article class="atelier">
         <?php if(have_posts()):the_post(); ?>
         <h1><?php the_title(); ?></h1>
-        <h2>Description de l'atelier</h2>
-        <section class="evenement__animateur">
-        <p>L'animateur de l'atelier <div class="contenuField"><?php the_field('animateur');?></div></p>
-        </section>
-        <section class="evenement__local">
-        <p> L'animateur de l'atelier :<div class="contenuField"><?php the_field('local_ou_se_deroulera_latelier');?></div></p>
-        </section>
-        <section class="evenement__resume">
-            <div class="contenuField"><?php the_field('description');?>
-        </section>
+        <h3>Description de l'atelier</h3>
+            <section class="evenement__animateur">
+            <p>L'animateur de l'atelier <div class="contenuField"><?php the_field('animateur');?></div></p>
+            </section>
+            <section class="evenement__local">
+            <p> L'animateur de l'atelier :<div class="contenuField"><?php the_field('local_ou_se_deroulera_latelier');?></div></p>
+            </section>
+            <section class="evenement__resume">
+                <?php the_field('description');?>
+            </section>
         
         <h3>Horaire et dates de l'atelier</h3>
         <section class="evenement__duree">
-            <p>Durée de chacune des séances est de <div class="contenuField"><?php the_field('duree_dune_seance_datelier'); ?></div></p>
+                <p>Durée de chacune des séances est de <div class="contenuField"><?php the_field('duree_dune_seance_datelier'); ?></div></p>
         </section>
-       <section class="evenement__debut">
-            <p>Date de début  <div class="contenuField"><?php the_field('date_de_debut_de_latelier'); ?></div></p>
-       </section>
-       <section class="evenement__fin">
-            <p>Date de fin <div class="contenuField"><?php the_field('date_de_fin_de_latelier'); ?></div></p>    
-       </section>
-       <section class="evenement__jours">
-            <p> Date de fin <div class="contenuField"><?php the_field('jours_de_la_semaine_de_latelier'); ?></div></p>
-       </section>
-       <section class="evenement__horaire">
-            <div>l'horaire :<div class="contenuField"><?php the_field('heure_de_debut');?> <p> à </p><?php the_field('heure_de_fin');?></div></div>
-       </section>
+        <section class="evenement__debut">
+                <p>Date de début  <div class="contenuField"><?php the_field('date_de_debut_de_latelier'); ?></div></p>
+        </section>
+        <section class="evenement__fin">
+                <p>Date de fin <div class="contenuField"><?php the_field('date_de_fin_de_latelier'); ?></div></p>    
+        </section>
+        <section class="evenement__jours">
+                <p>La formation se donnera : <div class="contenuField"><?php the_field('jours_de_la_semaine_de_latelier'); ?></div></p>
+        </section>
+        <section class="evenement__horaire">
+                <div>l'horaire :<div class="contenuField"><?php the_field('heure_de_debut');?> <p> à </p><?php the_field('heure_de_fin');?></div></div>
+        </section>
 
         <?php endif; ?>
     </article>
